9d212b8425ef07c213c2ed9d4354483cdb50a0b9
Lim Lai Ho <laiho.lim.work@gmail.com>
2025-01-05 13:35:26 +0800

    feat: add relationships between `Item` and `ModifierGroup`

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,6 @@ class Item < ApplicationRecord
   has_one :section, through: :section_item
   has_one :modifier
   has_one :modifier_group, through: :modifier
+  has_many :item_modifier_groups
+  has_many :modifier_groups, through: :item_modifier_groups
 end
--- app/models/modifier_group.rb
+++ app/models/modifier_group.rb
@@ -3,4 +3,6 @@
 class ModifierGroup < ApplicationRecord
   has_many :modifiers
   has_many :items, through: :modifiers
+  has_one :item_modifier_group
+  has_one :item, through: :item_modifier_group
 end
