391b01301b7115a521deabd969669b974bcbf446
Lim Lai Ho <laiho.lim.work@gmail.com>
2025-01-05 12:47:12 +0800

    feat: add relationships between `Item`, `SectionItem` and `Section`

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  has_one :section_item
+  has_one :section, through: :section_item
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -3,4 +3,6 @@
 class Section < ApplicationRecord
   has_one :menu_section
   has_one :menu, through: :menu_section
+  has_many :section_items
+  has_many :items, through: :section_items
 end
